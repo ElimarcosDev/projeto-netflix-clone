6109f6c4422b6e35f6e4c79833527cfcabeb9059
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-12-17 14:20:38 -0300

    projeto netflix

---
+++ javatwd.js
@@ -0,0 +1,111 @@
+function temp1(){
+    var episodios = document.querySelector('#episodios1')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp2(){
+    var episodios = document.querySelector('#episodios2')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp3(){
+    var episodios = document.querySelector('#episodios3')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp4(){
+    var episodios = document.querySelector('#episodios4')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+
+function temp5(){
+    var episodios = document.querySelector('#episodios5')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp6(){
+    var episodios = document.querySelector('#episodios6')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp7(){
+    var episodios = document.querySelector('#episodios7')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp8(){
+    var episodios = document.querySelector('#episodios8')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp9(){
+    var episodios = document.querySelector('#episodios9')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp10(){
+    var episodios = document.querySelector('#episodios10')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
+function temp11(){
+    var episodios = document.querySelector('#episodios11')
+
+    if ( episodios.style.display == 'inline-block'){
+        episodios.style.display = 'none'
+    }
+    else{
+        episodios.style.display = 'inline-block'
+    }
+}
